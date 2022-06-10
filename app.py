af0fdeaccf46defb1ea1c92e56133fae2d572ac3
Nisflei <ncgaloni@hotmail.com>
2022-06-09 21:06:08 -0300

    versaoInicial

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask;
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'BEM VINDO AO MUNDO... DEVOPS.!!!'
+
+@app.route('/msg')
+def msg():
+    return 'Usando os conceitos de deploy..!!!'
+
+app.run();
