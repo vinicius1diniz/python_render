e2974c57d631d3ab801eb51ac3e32b7493c4c13f
Nisflei Galoni <88749138+Nisflei@users.noreply.github.com>
2023-06-13 14:03:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'BEM VINDO AO MUNDO... DEVOPS.!!! -- Version: 0.3'
+    return '<center><h1>BEM VINDO AO MUNDO... DEVOPS.!!! -- Version: 0.3</h1></center>'
 
 @app.route('/msg')
 def msg():
-    return 'Usando os conceitos de deploy..!!!'
+    return '<center><h1>Usando os conceitos de deploy..!!!</h1></center>'
 
 #app.run();
